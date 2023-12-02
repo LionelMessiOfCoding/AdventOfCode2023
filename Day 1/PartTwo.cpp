520f8f5f4f4c9ddf4a7ee1b94247c49228b965fc
LionelMessiOfCoding <baldeep_uppal_999@hotmail.com>
2023-12-02 11:33:58 +1100

    First commit

---
+++ Day 1/PartTwo.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+int main() {
+
+    std::unordered_map<std::string, int> word_numbers = 
+    {
+        {"one", 1}, {"two", 2}, {"three", 3}, {"four", 4}, {"five", 5},
+        {"six", 6}, {"seven", 7}, {"eight", 8}, {"nine", 9}
+    };
+
+    std::ifstream data ("data.txt");
+    std::string line;
+
+    if(data.is_open()) {
+        int answer = 0;
+        // Get the first/last digit values as before, as well as their respective index
+        // Find all occurrences of word_numbers in line and compare index
+        while(getline(data, line)) {
+            int first = -1, last = -1, first_index = -1, last_index = -1;
+
+            for(int i = 0; i < line.size(); i++) {
+                if(!isdigit(line[i]))
+                    continue;
+                if(first == -1) {
+                    first = line[i] -48;
+                    first_index = i;
+                    last = line[i] -48;
+                    last_index = i;
+                } else {
+                    last = line[i] -48;
+                    last_index = i;
+                }
+            }
+
+            for(const auto& pair : word_numbers) {
+                if(line.find(pair.first) == std::string::npos) // find() returns npos if not found
+                    continue;
+                int word_index = line.find(pair.first);
+                if(word_index < first_index) {
+                    first = pair.second;
+                    first_index = word_index;
+                }
+                if(line.rfind(pair.first) == std::string::npos) continue;
+                word_index = line.rfind(pair.first);
+                if(word_index > last_index) {
+                    last = pair.second;
+                    last_index = word_index;
+                }
+            }
+
+            answer += first*10 + last;
+        }
+        data.close();
+        std::cout << "The sum of all calibration values is " << answer;
+    }
+
+    return 0;
+}
+
+// --- Part Two ---
+// Your calculation isn't quite right. It looks like some of the digits are actually spelled out with letters: one, two, three, four, five, six, seven, eight, and nine also count as valid "digits".
+
+// Equipped with this new information, you now need to find the real first and last digit on each line. For example:
+
+// two1nine
+// eightwothree
+// abcone2threexyz
+// xtwone3four
+// 4nineeightseven2
+// zoneight234
+// 7pqrstsixteen
+// In this example, the calibration values are 29, 83, 13, 24, 42, 14, and 76. Adding these together produces 281.
+
+// What is the sum of all of the calibration values?
---
+++ Day 1/PartTwo_attempt2.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+int main() {
+
+    std::unordered_map<std::string, std::string> word_numbers = 
+    {
+        {"one", "1"}, {"two", "2"}, {"three", "3"}, {"four", "4"}, {"five", "5"},
+        {"six", "6"}, {"seven", "7"}, {"eight", "8"}, {"nine", "9"}
+    };
+
+    std::ifstream data ("data.txt");
+    std::string line;
+
+    if(data.is_open()) {
+        int answer = 0;
+        // Find all occurrences of word_numbers in line and replace second char with digit
+        // Iterate over line and get first/last digit
+        while(getline(data, line)) {
+            for(const auto& pair : word_numbers) {
+                if(line.find(pair.first) == std::string::npos) // find() returns npos if not found
+                    continue;
+                int word_index = line.find(pair.first);
+                line.replace(word_index+1, 1, pair.second);
+
+                if(line.rfind(pair.first) == std::string::npos)
+                    continue;
+                word_index = line.rfind(pair.first);
+                line.replace(word_index+1, 1, pair.second);
+            }
+            
+            int first = -1, last = -1;
+
+            for(int i = 0; i < line.size(); i++) {
+                if(!isdigit(line[i]))
+                    continue;
+                if(first == -1) {
+                    first = line[i] -48;
+                    last = line[i] -48;
+                } else {
+                    last = line[i] -48;
+                }
+            }
+
+            
+            answer += first*10 + last;
+        }
+        data.close();
+        std::cout << "The sum of all calibration values is " << answer;
+    }
+
+    return 0;
+}
+
+// --- Part Two ---
+// Your calculation isn't quite right. It looks like some of the digits are actually spelled out with letters: one, two, three, four, five, six, seven, eight, and nine also count as valid "digits".
+
+// Equipped with this new information, you now need to find the real first and last digit on each line. For example:
+
+// two1nine
+// eightwothree
+// abcone2threexyz
+// xtwone3four
+// 4nineeightseven2
+// zoneight234
+// 7pqrstsixteen
+// In this example, the calibration values are 29, 83, 13, 24, 42, 14, and 76. Adding these together produces 281.
+
+// What is the sum of all of the calibration values?
